8e1345a1f76db8b17bccb3c25747fb172fcc023b
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-06-20 19:31:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function showMessage()
+{
+	let text=document.getElementById("text").value;
+	let delayvalue=document.getElementById("delay").value;
+
+	return;
+}
+await delay(delayvalue);
+ // Display the message
+  document.getElementById('output').innerText = text;
+}
+// Attach event listener to button
+document.getElementById('btn').addEventListener('click', showMessage);
